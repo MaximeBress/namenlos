6c1a05e1d91e2ac68dfc4369d0c314205f2545fe
mbressange <maxime.bressange@preconys.com>
2025-09-26 11:01:29 +0200

    Change image height

--- src/app/projets/[uid]/page.tsx
+++ src/app/projets/[uid]/page.tsx
@@ -13,8 +13,8 @@ export default async function Page({ params }: { params: Promise<Params> }) {
   const project = await client.getByUID("projet", uid).catch(() => notFound());
 
   return (
-    <div className="container flex h-[calc(100vh-152px)] max-w-7xl flex-col pt-10 md:flex-row-reverse lg:container lg:mx-auto">
-      <div className="flex flex-col gap-5 px-6 md:w-1/2 md:overflow-hidden lg:w-2/5 lg:px-3">
+    <div className="container flex h-[calc(100vh-80px)] max-w-7xl flex-col gap-4 pt-5 md:flex-row-reverse lg:container lg:mx-auto">
+      <div className="flex flex-col gap-5 px-6 md:w-1/2 md:overflow-hidden lg:w-2/5 lg:px-0">
         <TextProject project={project.data} />
       </div>
       <div className="flex flex-1 md:overflow-hidden">
